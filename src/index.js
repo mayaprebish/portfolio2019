4e85e6066ef8853d4e1a5bc79a05cc1273d3e1a6
Maya Prebish <prebish.m@husky.neu.edu>
2019-10-10 01:31:11 -0400

    latest

--- src/index.js
+++ src/index.js
@@ -16,11 +16,11 @@ class App extends React.Component {
             <div>
                 <Header/>
 
-                <Route exact path='/' component={About}/>
-                <Route path='/about' component={About}/>
-                <Route path='/contact' component={Contact}/>
-                <Route path='/portfolio' component={Portfolio}/>
-                <Route path='/resume' component={Resume}/>
+                <Route exact path='/' component={About} id='nav-about'/>
+                <Route path='/about' component={About} id='nav-about'/>
+                <Route path='/portfolio' component={Portfolio} id='nav-portfolio'/>
+                <Route path='/contact' component={Contact} id='nav-contact'/>
+                <Route path='/resume' component={Resume} id='nav-resume'/>
             </div>
         </Router>
     }
@@ -34,6 +34,26 @@ class About extends React.Component {
     render() {
         return <div className="container">
             <h1>about me</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
         </div>
     }
 }
@@ -80,24 +100,63 @@ class Header extends React.Component {
     }
 
     render() {
-        return <div className="container-fluid header">
-            <h1>Maya Prebish</h1>
-            <p>
-                I am a second year CS student at Northeastern University's Khoury College of Computer Sciences,
-                looking to expand my knowledge of
-                the field and gain experience, especially in software engineering and web development.
-            </p>
+        return <div className="header">
+            <div className="jumbotron jumbotron-fluid bg-dark" style={jumboStyle}>
+                <div className="container">
 
-            <p>
-                Please note this site is a work in progress as of October 2019.
-            </p>
+                    <h1 className="display-4">Maya Prebish</h1>
 
-            <Link to='/about'>about</Link>
-            <Link to='/contact'>contact</Link>
-            <Link to='/portfolio'>portfolio</Link>
-            <Link to='/resume'>resume</Link>
+                    <div className="row">
+                        <div className="col">
+                            <p className="lead">I am a second year CS student at Northeastern University's Khoury
+                                College of Computer Sciences, looking to expand my knowledge of the field and
+                                gain experience, especially in software engineering and web development.</p>
+                            <p className="lead"> Please note this site is a work in progress as of October 2019.</p>
+                        </div>
+                        <div className="col">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link to='/portfolio' className='nav-link'>Portfolio</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/about' className='nav-link'>About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/contact' className='nav-link'>Contact</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/resume' className='nav-link'>Resume</Link>
+                    </li>
+                </ul>
+            </nav>
+
+
+
         </div>
     }
+}
+
+const jumboStyle = {
+    padding: '20px',
+    margin: '0px',
+    color: 'white'
+}
+
+const navbarStyle = {
+    marginLeft: '50px',
+    padding: '0px',
+    margin: '0px'
+}
+
+const buttonStyle = {
+    margin: '10px',
+    borderRadius: '0px'
 }
 
 ReactDOM.render(
