7811b180593aac3b23fa6c305b5a265c71ce1e18
Maya Prebish <prebish.m@husky.neu.edu>
2019-10-13 10:44:37 -0400

    about update

--- src/index.js
+++ src/index.js
@@ -78,88 +78,94 @@ class About extends React.Component {
         return <div>
             <div className="container">
                 <div className="row" style={listStyle}>
-                    <div className="col-4">
-                        <img src="https://live.staticflickr.com/65535/48882028043_081b73c769_o.jpg"
-                             alt="about photo"
-                             width="100%"
-                             className="photo"/>
-                    </div>
-                    <div className="col-8 text-left">
-                        <h3>About Me</h3>
-                        <p>I am a second-year at Northeastern University seeking a BA in Computer Science. My topics of
-                            interest in the CS field are software engineering and web development. I've been involved in
-                            programming since high school, but have always been fascinated by the history and
-                            development of
-                            modern computing.</p>
+                    <div className="row">
+                        <div className="col-4 text-justify">
+                            <img src="https://live.staticflickr.com/65535/48882028043_081b73c769_o.jpg"
+                                 alt="about photo"
+                                 width="100%"
+                                 className="photo"/>
+                            <h3>About Me</h3>
+                            <p>I am a second-year at Northeastern University seeking a BA in Computer Science. My topics
+                                of
+                                interest in the CS field are software engineering and web development. I've been
+                                involved in
+                                programming since high school, but have always been fascinated by the history and
+                                development of
+                                modern computing.</p>
 
-                        <p>I'm a thorough worker who picks up new languages quickly. I love to learn, and I thrive in
-                            environments where I'm immersed in topics that are brand new to me. The primary traits I
-                            have to offer in a professional environment are my ability to communicate, my ability to
-                            work independently, and my consistency and attention to detail.</p>
-                    </div>
-                </div>
+                            <p>I'm a thorough worker who picks up new languages quickly. I love to learn, and I thrive
+                                in
+                                environments where I'm immersed in topics that are brand new to me. The primary traits I
+                                have to offer in a professional environment are my ability to communicate, my ability to
+                                work independently, and my consistency and attention to detail.</p>
+                        </div>
+                        <div className="col-4">
+                            <img src="https://live.staticflickr.com/65535/48883505998_cd7d028f9b_o.png"
+                                 alt="about photo"
+                                 width="100%"
+                                 className="photo"/>
+                            <h3>Academics</h3>
+                            <p>GPA: 3.0/4</p>
+                            <p>Relevant Coursework: Web Development*, Algorithms & Data*, Computer Systems*,
+                                Object-Oriented
+                                Design, Discrete Structures, Fundamentals of Computer Science, Calculus 1 & 2&nbsp;
+                                <i>(*Currently Enrolled)</i></p>
+                            <p>*Unofficial transcript available upon request</p>
 
-                <div className="row" style={listStyle}>
-                    <div className="col-8 text-right">
-                        <h3>Interests</h3>
+                        </div>
+                        <div className="col-4 text-justify">
+                            <img src="https://live.staticflickr.com/65535/48882240643_bdcf5595cd_o.png"
+                                 alt="about photo"
+                                 width="100%"
+                                 className="photo"/>
+                            <h3>Interests</h3>
 
-                        <p>Outside of school, my biggest passion is music, and I play with my friends whenever
-                            I get the chance. My instruments of choice are guitar, bass and drums, and I played in
-                            several bands throughout high school. I also helped host several live concerts a year
-                            through my club, Live Expression.</p>
+                            <p>Outside of school, my biggest passion is music. My instruments of choice are guitar, bass
+                                and drums, and I played in bands throughout high school.</p>
 
-                        <p>My next favorite activity is gaming. I like to stay updated on the gaming industry and I've
-                            dabbled in videogame programming in and out of school. My favorite genres are RPG games
-                            (Skyrim,
-                            Fable, Fire Emblem), puzzle/adventure games (Legend of Zelda, Pokemon) and casual/relaxing
-                            games
-                            (Animal Crossing, Stardew Valley).</p>
+                            <p>My next favorite activity is gaming. I like to stay updated on the gaming industry and
+                                I've
+                                dabbled in videogame programming in and out of school. My favorite genres are RPG games
+                                (Skyrim,
+                                Fable, Fire Emblem), puzzle/adventure games (Legend of Zelda, Pokemon) and
+                                casual/relaxing
+                                games
+                                (Animal Crossing, Stardew Valley).</p>
 
-                        <p>I'm also interested in digital art. I've always liked to draw, and I like making sketches
-                            of people and places from my life. I hope to be a skilled digital artist at some point
-                            in the future.</p>
-                    </div>
-                    <div className="col-4">
-                        <img src="https://live.staticflickr.com/65535/48882240643_bdcf5595cd_o.png"
-                             alt="about photo"
-                             width="100%"
-                             className="photo"/>
-                    </div>
-                </div>
+                            <p>I'm also interested in digital art. I've always liked to draw, and I like making sketches
+                                of people and places from my life. I hope to be a skilled digital artist at some point
+                                in the future.</p>
+                        </div>
 
-                <div className="row" style={listStyle}>
-                    <div className="col-4">
-                        <img src="https://live.staticflickr.com/65535/48883505998_cd7d028f9b_o.png"
-                             alt="about photo"
-                             width="100%"
-                             className="photo"/>
-                    </div>
-                    <div className="col-8 text-left">
-                        <h3>Academics</h3>
-                        <p><b>GPA: 3.0/4</b></p>
-                        <p>Relevant Coursework: Web Development*, Algorithms & Data*, Computer Systems*, Object-Oriented
-                            Design, Discrete Structures, Fundamentals of Computer Science, Calculus 1 & 2&nbsp;
-                            <i>(*Currently Enrolled)</i></p>
-                        <p>*Unofficial transcript available upon request</p>
                     </div>
                 </div>
 
+                <footer
+                    className="nav"
+                    style={{marginTop: '200px'}}>
+                    <div className="container">
+                        <p
+                            className="float-right"
+                            style={
+                                {
+                                    fontSize: 14
+                                }
+                            }>Image sources:&nbsp;
+                            <a href="https://i2.wp.com/cornellsun.com/wp-content/uploads/2017/01/alabama-shakes.jpg?fit=600%2C600">Sound
+                                & Color, </a>
+                            <a
+                                href="https://thehub.musiciansfriend.com/images/interviews/Courtney-Barnett-Header.jpg"> Courtney
+                                Barnett,
+                            </a>
+                            <a href="https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2015/11/link-zelda-1200x600.png">Legend
+                                of Zelda, </a>
+                            <a href="https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Northeastern_University_seal.svg/1200px-Northeastern_University_seal.svg.png">
+                                Northeastern Logo
+                            </a>
+                        </p>
+                    </div>
+                </footer>
             </div>
-            <footer className="nav" style={{marginTop: '200px'}}>
-                <div className="container">
-                    <p className="float-right" style={{fontSize: 14}}>Image sources:&nbsp;
-                        <a href="https://i2.wp.com/cornellsun.com/wp-content/uploads/2017/01/alabama-shakes.jpg?fit=600%2C600">Sound
-                            & Color, </a>
-                        <a href="https://thehub.musiciansfriend.com/images/interviews/Courtney-Barnett-Header.jpg">Courtney
-                            Barnett, </a>
-                        <a href="https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2015/11/link-zelda-1200x600.png">Legend
-                            of Zelda, </a>
-                        <a href="https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Northeastern_University_seal.svg/1200px-Northeastern_University_seal.svg.png">
-                            Northeastern Logo
-                        </a>
-                    </p>
-                </div>
-            </footer>
         </div>
     }
 }
@@ -384,60 +390,60 @@ class Excellence extends React.Component {
                 </p>
                 <p>Sample input: "toh-3.txt"</p>
                 <pre>canvas 145 50 410 220
-shape disk1 rectangle
-shape disk2 rectangle
-shape disk3 rectangle
-motion disk1 1 190 180 20 30 0 49 90  1 190 180 20 30 0 49 90
-motion disk1 1 190 180 20 30 0 49 90  25 190 180 20 30 0 49 90
-motion disk2 1 167 210 65 30 6 247 41  1 167 210 65 30 6 247 41
-motion disk2 1 167 210 65 30 6 247 41  57 167 210 65 30 6 247 41
-motion disk3 1 145 240 110 30 11 45 175  1 145 240 110 30 11 45 175
-motion disk3 1 145 240 110 30 11 45 175  121 145 240 110 30 11 45 175
-motion disk1 25 190 180 20 30 0 49 90  35 190 50 20 30 0 49 90
-motion disk1 35 190 50 20 30 0 49 90  36 190 50 20 30 0 49 90
-motion disk1 36 190 50 20 30 0 49 90  46 490 50 20 30 0 49 90
-motion disk1 46 490 50 20 30 0 49 90  47 490 50 20 30 0 49 90
-motion disk1 47 490 50 20 30 0 49 90  57 490 240 20 30 0 49 90
-motion disk1 57 490 240 20 30 0 49 90  89 490 240 20 30 0 49 90
-motion disk2 57 167 210 65 30 6 247 41  67 167 50 65 30 6 247 41
-motion disk2 67 167 50 65 30 6 247 41  68 167 50 65 30 6 247 41
-motion disk2 68 167 50 65 30 6 247 41  78 317 50 65 30 6 247 41
-motion disk2 78 317 50 65 30 6 247 41  79 317 50 65 30 6 247 41
-motion disk2 79 317 50 65 30 6 247 41  89 317 240 65 30 6 247 41
-motion disk1 89 490 240 20 30 0 49 90  99 490 50 20 30 0 49 90
-motion disk2 89 317 240 65 30 6 247 41  185 317 240 65 30 6 247 41
-motion disk1 99 490 50 20 30 0 49 90  100 490 50 20 30 0 49 90
-motion disk1 100 490 50 20 30 0 49 90  110 340 50 20 30 0 49 90
-motion disk1 110 340 50 20 30 0 49 90  111 340 50 20 30 0 49 90
-motion disk1 111 340 50 20 30 0 49 90  121 340 210 20 30 0 49 90
-motion disk1 121 340 210 20 30 0 49 90  153 340 210 20 30 0 49 90
-motion disk3 121 145 240 110 30 11 45 175  131 145 50 110 30 11 45 175
-motion disk3 131 145 50 110 30 11 45 175  132 145 50 110 30 11 45 175
-motion disk3 132 145 50 110 30 11 45 175  142 445 50 110 30 11 45 175
-motion disk3 142 445 50 110 30 11 45 175  143 445 50 110 30 11 45 175
-motion disk3 143 445 50 110 30 11 45 175  153 445 240 110 30 11 45 175
-motion disk1 153 340 210 20 30 0 49 90  163 340 50 20 30 0 49 90
-motion disk3 153 445 240 110 30 11 45 175  161 445 240 110 30 0 255 0
-motion disk3 161 445 240 110 30 0 255 0  302 445 240 110 30 0 255 0
-motion disk1 163 340 50 20 30 0 49 90  164 340 50 20 30 0 49 90
-motion disk1 164 340 50 20 30 0 49 90  174 190 50 20 30 0 49 90
-motion disk1 174 190 50 20 30 0 49 90  175 190 50 20 30 0 49 90
-motion disk1 175 190 50 20 30 0 49 90  185 190 240 20 30 0 49 90
-motion disk1 185 190 240 20 30 0 49 90  217 190 240 20 30 0 49 90
-motion disk2 185 317 240 65 30 6 247 41  195 317 50 65 30 6 247 41
-motion disk2 195 317 50 65 30 6 247 41  196 317 50 65 30 6 247 41
-motion disk2 196 317 50 65 30 6 247 41  206 467 50 65 30 6 247 41
-motion disk2 206 467 50 65 30 6 247 41  207 467 50 65 30 6 247 41
-motion disk2 207 467 50 65 30 6 247 41  217 467 210 65 30 6 247 41
-motion disk1 217 190 240 20 30 0 49 90  227 190 50 20 30 0 49 90
-motion disk2 217 467 210 65 30 6 247 41  225 467 210 65 30 0 255 0
-motion disk2 225 467 210 65 30 0 255 0  302 467 210 65 30 0 255 0
-motion disk1 227 190 50 20 30 0 49 90  228 190 50 20 30 0 49 90
-motion disk1 228 190 50 20 30 0 49 90  238 490 50 20 30 0 49 90
-motion disk1 238 490 50 20 30 0 49 90  239 490 50 20 30 0 49 90
-motion disk1 239 490 50 20 30 0 49 90  249 490 180 20 30 0 49 90
-motion disk1 249 490 180 20 30 0 49 90  257 490 180 20 30 0 255 0
-                    motion disk1 257 490 180 20 30 0 255 0  302 490 180 20 30 0 255 0</pre>
+            shape disk1 rectangle
+            shape disk2 rectangle
+            shape disk3 rectangle
+            motion disk1 1 190 180 20 30 0 49 90  1 190 180 20 30 0 49 90
+            motion disk1 1 190 180 20 30 0 49 90  25 190 180 20 30 0 49 90
+            motion disk2 1 167 210 65 30 6 247 41  1 167 210 65 30 6 247 41
+            motion disk2 1 167 210 65 30 6 247 41  57 167 210 65 30 6 247 41
+            motion disk3 1 145 240 110 30 11 45 175  1 145 240 110 30 11 45 175
+            motion disk3 1 145 240 110 30 11 45 175  121 145 240 110 30 11 45 175
+            motion disk1 25 190 180 20 30 0 49 90  35 190 50 20 30 0 49 90
+            motion disk1 35 190 50 20 30 0 49 90  36 190 50 20 30 0 49 90
+            motion disk1 36 190 50 20 30 0 49 90  46 490 50 20 30 0 49 90
+            motion disk1 46 490 50 20 30 0 49 90  47 490 50 20 30 0 49 90
+            motion disk1 47 490 50 20 30 0 49 90  57 490 240 20 30 0 49 90
+            motion disk1 57 490 240 20 30 0 49 90  89 490 240 20 30 0 49 90
+            motion disk2 57 167 210 65 30 6 247 41  67 167 50 65 30 6 247 41
+            motion disk2 67 167 50 65 30 6 247 41  68 167 50 65 30 6 247 41
+            motion disk2 68 167 50 65 30 6 247 41  78 317 50 65 30 6 247 41
+            motion disk2 78 317 50 65 30 6 247 41  79 317 50 65 30 6 247 41
+            motion disk2 79 317 50 65 30 6 247 41  89 317 240 65 30 6 247 41
+            motion disk1 89 490 240 20 30 0 49 90  99 490 50 20 30 0 49 90
+            motion disk2 89 317 240 65 30 6 247 41  185 317 240 65 30 6 247 41
+            motion disk1 99 490 50 20 30 0 49 90  100 490 50 20 30 0 49 90
+            motion disk1 100 490 50 20 30 0 49 90  110 340 50 20 30 0 49 90
+            motion disk1 110 340 50 20 30 0 49 90  111 340 50 20 30 0 49 90
+            motion disk1 111 340 50 20 30 0 49 90  121 340 210 20 30 0 49 90
+            motion disk1 121 340 210 20 30 0 49 90  153 340 210 20 30 0 49 90
+            motion disk3 121 145 240 110 30 11 45 175  131 145 50 110 30 11 45 175
+            motion disk3 131 145 50 110 30 11 45 175  132 145 50 110 30 11 45 175
+            motion disk3 132 145 50 110 30 11 45 175  142 445 50 110 30 11 45 175
+            motion disk3 142 445 50 110 30 11 45 175  143 445 50 110 30 11 45 175
+            motion disk3 143 445 50 110 30 11 45 175  153 445 240 110 30 11 45 175
+            motion disk1 153 340 210 20 30 0 49 90  163 340 50 20 30 0 49 90
+            motion disk3 153 445 240 110 30 11 45 175  161 445 240 110 30 0 255 0
+            motion disk3 161 445 240 110 30 0 255 0  302 445 240 110 30 0 255 0
+            motion disk1 163 340 50 20 30 0 49 90  164 340 50 20 30 0 49 90
+            motion disk1 164 340 50 20 30 0 49 90  174 190 50 20 30 0 49 90
+            motion disk1 174 190 50 20 30 0 49 90  175 190 50 20 30 0 49 90
+            motion disk1 175 190 50 20 30 0 49 90  185 190 240 20 30 0 49 90
+            motion disk1 185 190 240 20 30 0 49 90  217 190 240 20 30 0 49 90
+            motion disk2 185 317 240 65 30 6 247 41  195 317 50 65 30 6 247 41
+            motion disk2 195 317 50 65 30 6 247 41  196 317 50 65 30 6 247 41
+            motion disk2 196 317 50 65 30 6 247 41  206 467 50 65 30 6 247 41
+            motion disk2 206 467 50 65 30 6 247 41  207 467 50 65 30 6 247 41
+            motion disk2 207 467 50 65 30 6 247 41  217 467 210 65 30 6 247 41
+            motion disk1 217 190 240 20 30 0 49 90  227 190 50 20 30 0 49 90
+            motion disk2 217 467 210 65 30 6 247 41  225 467 210 65 30 0 255 0
+            motion disk2 225 467 210 65 30 0 255 0  302 467 210 65 30 0 255 0
+            motion disk1 227 190 50 20 30 0 49 90  228 190 50 20 30 0 49 90
+            motion disk1 228 190 50 20 30 0 49 90  238 490 50 20 30 0 49 90
+            motion disk1 238 490 50 20 30 0 49 90  239 490 50 20 30 0 49 90
+            motion disk1 239 490 50 20 30 0 49 90  249 490 180 20 30 0 49 90
+            motion disk1 249 490 180 20 30 0 49 90  257 490 180 20 30 0 255 0
+            motion disk1 257 490 180 20 30 0 255 0  302 490 180 20 30 0 255 0</pre>
                 <p>Output for this file:</p>
                 <img src="https://media.giphy.com/media/U1ln9SFapdUn6HXSbw/source.gif"/>
             </div>
@@ -451,7 +457,7 @@ class Minesweeper extends React.Component {
             <div className="container">
                 <h2>Minesweeper</h2>
                 <p>I created a replica of Minesweeper for Fundamentals of Computer Science II with partner Nicole
-                    Danuwidjaja. We used a breadth-first  search algorithm to maintain state in the puzzle and implement
+                    Danuwidjaja. We used a breadth-first search algorithm to maintain state in the puzzle and implement
                     the "flooding" behavior.</p>
                 <p>Please <Link to='/contact'>contact me </Link>for access to the GitHub repository for this program.
                 </p>
